354decd173433ab1160b5ee9dd49724e8c5095fd
VIESAFE <vesf.ti@gmail.com>
2023-06-12 21:22:45 -0300

    'arquivo.csv'

--- Cadastro/cadastro_corrigido.py
+++ Cadastro/cadastro_corrigido.py
@@ -1,74 +1,78 @@
+import csv
+
 class Cadastro:
     def __init__(self):
         self.items = {}
-        self.email = ""
 
     def criar_cadastroProf(self):
-        self.email = input("Digite seu email: ")
+        email = input("Digite seu email: ")
         senha = input("Digite sua senha: ")
-        if self.email in self.items:
+        
+        if email in self.items:
             print("Email já existe")
         else:
-            self.items[self.email] = []
+            self.items[email] = [senha, None, None]
             print("Email cadastrado com sucesso")
             try:
-                self.salvar_cadastro(senha)
+                self.salvar_cadastro()
             except Exception as e:
                 print("Erro ao salvar cadastro:", str(e))
 
-    def salvar_cadastro(self, senha):
+    def salvar_cadastro(self):
         try:
-            with open("cadastro_data.txt", "a") as arquivo:
-                arquivo.write(f"Email: {self.email}\tSenha: {senha}\n")
+            with open("cadastro_data.csv", "w", newline="") as arquivo:
+                writer = csv.writer(arquivo)
+                for email, dados in self.items.items():
+                    writer.writerow([email] + dados)
         except Exception as e:
             print("Erro ao salvar cadastro:", str(e))
 
     def carregar_cadastros(self):
         try:
-            with open("cadastro_data.txt", "r") as arquivo:
-                for linha in arquivo:
-                    values = linha.strip().split("\t")
-                    if len(values) == 2:
-                        email, senha = values
-                        self.items[email] = []
-                        print("Cadastro carregado com sucesso!")
+            with open("cadastro_data.csv", "r") as arquivo:
+                reader = csv.reader(arquivo)
+                for linha in reader:
+                    email = linha[0]
+                    dados = linha[1:]
+                    self.items[email] = dados
+                    print("Cadastro carregado com sucesso!")
         except FileNotFoundError:
-            # Caso o arquivo não exista, cria um novo arquivo vazio
-            with open("cadastro_data.txt", "w") as arquivo:
+            with open("cadastro_data.csv", "w", newline="") as arquivo:
                 pass
 
     def cadastro_turma(self):
         try:
+            email = input("Digite seu email: ")
+            if email not in self.items:
+                print("Email não encontrado.")
+                return
+
             qnt_alunos = int(input("Quantos alunos vão participar da turma? "))
+            codigo = input("Digite o código da turma: ")
+            self.items[email][1] = qnt_alunos
+            self.items[email][2] = codigo
+            print("Código criado com sucesso!")
+            try:
+                self.salvar_cadastro()
+            except Exception as e:
+                print("Erro ao salvar cadastro da turma:", str(e))
         except ValueError:
             print("Quantidade de alunos inválida. Digite um número inteiro.")
             return
 
-        codigo = input("Digite o código da turma: ")
-        self.items[self.email] = [qnt_alunos, codigo]
-        print("Código criado com sucesso!")
-        try:
-            self.salvar_cadastro_turma(qnt_alunos, codigo)
-        except Exception as e:
-            print("Erro ao salvar cadastro da turma:", str(e))
-
-    def salvar_cadastro_turma(self, qnt_alunos, codigo):
-        try:
-            with open("cadastro_data.txt", "a") as arquivo:
-                arquivo.write(f"Email: {self.email}\tQuantidade de Alunos: {qnt_alunos}\tCódigo: {codigo}\n")
-        except Exception as e:
-            print("Erro ao salvar cadastro da turma:", str(e))
-
     def verificar_codigo(self):
-        try:
-            codigo_digitado = ""
-            while codigo_digitado != self.items[self.email][1]:
-                codigo_digitado = input("Digite o código da turma: ")
-                if codigo_digitado != self.items[self.email][1]:
-                    print("Código inválido!")
-            print("Entrada com sucesso!")
-        except KeyError:
+        email = input("Digite seu email: ")
+        if email not in self.items:
             print("Email não encontrado.")
+            return
+
+        codigo_digitado = ""
+        while codigo_digitado != self.items[email][2]:
+            codigo_digitado = input("Digite o código da turma: ")
+            if codigo_digitado != self.items[email][2]:
+                print("Código inválido!")
+        print("Entrada com sucesso!")
+
 
 # Exemplo de uso:
 while True:
